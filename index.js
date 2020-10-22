167d0064afb0a98ae05222cf76348adf31b7df90
LimitToInfinity <21stLimitless@gmail.com>
2020-10-22 11:03:49 -0600

    Displays tv shows in JavaScript and adds styling to header and tv shows using flex.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const baseURL = 'https://api.tvmaze.com';
 const showsURL = `${baseURL}/shows`;
 
+const tvShowsContainer = document.querySelector('.tv-shows-container');
+
 fetch(showsURL)
   .then(parseJSON)
-  .then(console.log)
+  .then(renderTvShows);
+
+function renderTvShows(tvShows) {
+  tvShows.forEach(displayTvShow);
+}
+
+function displayTvShow(tvShow) {
+  const tvShowCard = document.createElement('div');
+  tvShowCard.classList.add('tv-show-card');
+
+  const tvShowName = document.createElement('h2');
+  tvShowName.textContent = tvShow.name;
+
+  const tvShowImage = document.createElement('img');
+  tvShowImage.src = tvShow.image.medium;
+
+  tvShowCard.append(tvShowName, tvShowImage);
+  tvShowsContainer.append(tvShowCard);
+}
 
 function parseJSON(response) {
   return response.json();
